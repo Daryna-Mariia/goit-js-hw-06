fc7084e0abcb966a6c4a4e90aacee6d1f48f5d20
Daryna-Mariia <stankevychdaryna@gmail.com>
2023-03-28 11:54:44 +0300

    new one without bag

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
- 
+const formHtml = document.querySelector('.login-form');
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+
+  const email = formElements.email.value.trim();
+  const password = formElements.password.value;
+
+  const formData = {
+    email,
+    password,
+  };
+  if (email && password) {
+    console.log(formData);
+    event.currentTarget.reset();
+  } else {
+    alert('Необхідно заповнити всі поля!');
+  }
+}
+
