f54a2bcc61020c1f10b6037d606280ea2c697f46
Daryna-Mariia <stankevychdaryna@gmail.com>
2023-03-28 11:44:25 +0300

    work good

--- js/task-08.js
+++ js/task-08.js
@@ -14,4 +14,23 @@ obj[formHtml.email.name] = `${email.value}`;
 obj[formHtml.password.name] = `${password.value}`;
 console.log(obj);
     }
+formRef.addEventListener('submit', onFormSubmit);
 
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+  
+    const email = formElements.email.value.trim();
+    const password = formElements.password.value;
+  
+    const formData = {
+      email,
+      password,
+};
+if (email && password) {
+    console.log(formData);
+    event.currentTarget.reset();
+  } else {
+    alert('Необхідно заповнити всі поля!');
+  }
+}
