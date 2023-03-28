6ff906cdfd8a303a9f46062e91d9329addfd82e9
Daryna-Mariia <stankevychdaryna@gmail.com>
2023-03-28 12:00:05 +0300

    fix bag another try

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", onButtonSubmit);
+
+function onButtonSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (!(email.value.trim() && password.value.trim())) {
+    return alert(`Please fill in all the fields!`);
+  }
+
+  const result = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(result);
+
+  event.currentTarget.reset();
+}
 
